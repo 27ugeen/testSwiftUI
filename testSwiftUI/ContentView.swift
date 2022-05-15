c5119056335691e26da9758f620050d394002af3
27ugeen <27ugeen@gmail.com>
2022-05-15 11:32:06 +0300

    init commit

---
+++ testSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  testSwiftUI
+//
+//  Created by GiN Eugene on 15/5/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ testSwiftUI/testSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  testSwiftUIApp.swift
+//  testSwiftUI
+//
+//  Created by GiN Eugene on 15/5/2022.
+//
+
+import SwiftUI
+
+@main
+struct testSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
